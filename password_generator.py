0fbdfffba35226efeca90999b7206761f857d5c0
Rydrek Harris <111711155+Drekk22@users.noreply.github.com>
2026-05-12 19:25:36 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,13 +2,47 @@ import random
 import string 
 
 length = int(input( "Enter password length: "))
+use_letters = ( input ( "Include letter? (Yes/no):"))
+use_numbers = (input ("Include numbers? (Yes/no): "))
+use_symbols = ( input (" Include symbols? (Yes/no): ")).lower()
+characters = ""
 
-character = string.ascii_letters  + string.digits +  string.punctuation
+if characters == "":
+    print(" Choose at least one option.")
 
-password = ""
+
+
+
+
+
+
+if use_letters == "Yes":
+    characters += string.ascii_letters
+
+if use_numbers == "Yes": 
+    characters += string.digits 
+
+if use_symbols == "Yes": 
+    characters += string.punctuation
+
+else: 
+    password = ""    
 
 for i in range(length): 
-    password += random .choice(character)
+    password += random .choice(characters)
+ 
 
-    print("\nGenerated Password:")
-    print(password)
+    strength = ""
+
+if len(password) < 8:
+    strength = "weak"  
+
+elif len(password) <= 12:
+    strength = " Mid"
+
+else: 
+     strength = "Good"
+    
+
+print("\nGenerated Password:")
+print(password)
